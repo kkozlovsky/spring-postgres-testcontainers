32c6bb8d01bf1b9663802deeed93cc51e72d781e
Kirill Kozlovskiy <k.kozlovskiy@paysend.com>
2023-12-04 20:55:32 +0300

    Initial commit

---
+++ src/main/java/ru/kerporation/SpringPostgresTestcontainersApplication.java
@@ -0,0 +1,13 @@
+package ru.kerporation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgresTestcontainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostgresTestcontainersApplication.class, args);
+	}
+
+}
